db29f17141679103f0d293bafef08b57cabd83d5
Kyle <sdmd731@gmail.com>
2025-08-10 18:12:18 +0800

    first commit

---
+++ order_test.py
@@ -0,0 +1,43 @@
+from bitoproClient.bitopro_restful_client import BitoproRestfulClient
+import os
+import json
+import dotenv
+
+dotenv.load_dotenv()
+
+# 替換為你自己的 API Key 與 Secret
+api_key = os.getenv("BITOPRO_API_KEY")
+api_secret = os.getenv("BITOPRO_API_SECRET")
+
+client = BitoproRestfulClient(api_key, api_secret)
+
+# 查詢支援的交易幣別
+currencies = client.get_currencies()
+
+# 查詢交易對的價格
+# pairs_price = client.get_tickers(pair="ETH_USDT")
+
+# 查詢交易對的成交紀錄
+# pairs_trade = client.get_trades(pair="ETH_TWD")
+
+# 查詢帳戶餘額
+# balance = client.get_account_balance()
+
+# balance_dict = {}
+# for currency in balance["data"]:
+#     if currency["available"] != "0":
+#         if "balance" not in balance_dict:
+#             balance_dict["balance"] = {}
+#         balance_dict["balance"][currency["currency"]] = currency["available"]
+
+# with open("balance.json", "w") as f:
+#     json.dump(balance_dict, f, indent=4)
+
+# 查詢歷史加值紀錄
+history_deposit = client.get_deposit_history(currency="TWD")
+
+with open("history_deposit.json", "w") as f:
+    json.dump(history_deposit, f, indent=4)
+
+# print(history_deposit)
+
